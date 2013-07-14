d6426c99c847c8f2099809311c40cadfd4a71e46
bitmapdata <bitmapdata.com@gmail.com>
2013-07-14 10:14:03 +0900

    Modified header comment

--- MSTranslateVendor/MSTranslateAccessTokenRequester.h
+++ MSTranslateVendor/MSTranslateAccessTokenRequester.h
@@ -4,7 +4,7 @@
 //
 //  Software License Agreement (BSD License)
 //
-//  Copyright (c) 2013 Minseok Shim. All rights reserved.
+//  Copyright (c) 2013 SHIM MIN SEOK. All rights reserved.
 //
 //  Redistribution and use in source and binary forms, with or without
 //  modification, are permitted provided that the following conditions are met:
