f922216778871ee307d015c4e929baff289ef0e5
bitmapdata <bitmapdata.com@gmail.com>
2013-07-14 23:04:29 +0900

    Updated sample code

--- MSTranslateVendorDemo/ViewController.m
+++ MSTranslateVendorDemo/ViewController.m
@@ -22,63 +22,76 @@
     //Must be called before used to MSTranslateVendor
     [[MSTranslateAccessTokenRequester sharedRequester] requestSynchronousAccessToken:CLIENT_ID clientSecret:CLIENT_SECRET];
     
+    [self testMethod1];
+    
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)testMethod1
+{
     MSTranslateVendor *vendor = [[MSTranslateVendor alloc] init];
 
     [vendor requestTranslate:@"독도는 한국 영토 입니다." from:@"ko" to:@"en" blockWithSuccess:
      ^(NSString *translatedText)
-    {
-        NSLog(@"translatedText: %@", translatedText);
-    }
-    failure:^(NSError *error)
-    {
-        NSLog(@"error_translate: %@", error);
-    }];
+     {
+         NSLog(@"translatedText: %@", translatedText);
+     }
+                     failure:^(NSError *error)
+     {
+         NSLog(@"error_translate: %@", error);
+     }];
     
     [vendor requestDetectTextLanguage:@"독도는 한국 영토 입니다." blockWithSuccess:
      ^(NSString *language)
-    {
-        NSLog(@"language:%@", language);
-    }
-    failure:
+     {
+         NSLog(@"language:%@", language);
+     }
+                              failure:
      ^(NSError *error)
-    {
-        NSLog(@"error_language: %@", error);
-    }];
-    
+     {
+         NSLog(@"error_language: %@", error);
+     }];
+        
     [vendor requestSpeakingText:@"Dokdo is Korean territory." language:@"en" blockWithSuccess:
      ^(NSData *streamData)
      {
          NSError *error;
          /*
-            ****In ARC following code not working. is ARC bug.
-            
-            AVAudipPlayer *player = [[AVAudioPlayer alloc] initWithData:streamData error:&error];
-            [player play];
-            
-            how is solved? AVAudioPlayer set a property strong. refer a following code.
+          ****In ARC following code not working. is ARC bug.
+          
+          AVAudipPlayer *player = [[AVAudioPlayer alloc] initWithData:streamData error:&error];
+          [player play];
+          
+          how is solved? AVAudioPlayer set a property strong. refer a following code.
           */
          
          self.player = [[AVAudioPlayer alloc] initWithData:streamData error:&error];
          [_player play];
      }
-     failure:
+                        failure:
      ^(NSError *error)
      {
          NSLog(@"error_speak: %@", error);
      }];
     
     [vendor requestBreakSentences:@"Dokdo est un territoire de la République de Corée. Géographiquement situé dans l'est de la République de Corée. Historiquement, géographiquement Vonage Dokdo est clairement le territoire de la République de Corée." language:@"fr" blockWithSuccess:^(NSDictionary *sentencesDict){
-    
+        
         NSLog(@"sentences_dict:%@", sentencesDict);
-    
+        
     }
-    failure:^(NSError *error)
-    {
+                          failure:^(NSError *error)
+     {
+         
+     }];
+    
+    [vendor requestTranslateArray:@[@"만나서 반갑습니다.", @"이 라이브러리가 당신에게 조금이나마 도움이 되기를 바랍니다.", @"최선을 다하겠습니다. 감사합니다."] from: @"ko" to:@"en" blockWithSuccess:^(NSArray *translatedTextArray) {
+        
+        NSLog(@"translatedTextArray:%@", translatedTextArray);
+    } failure:^(NSError *error) {
         
     }];
-    
-    [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+
 }
 
 - (void)didReceiveMemoryWarning
